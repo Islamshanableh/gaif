28597340d9dbf950688ad616e7362cd0b5770dfd
islamShanable <islam.sh@nadsoft.net>
2026-01-26 20:54:42 +0300

    enhance validation

--- validations/accommodation.validation.js
+++ validations/accommodation.validation.js
@@ -3,18 +3,13 @@ const Joi = require('joi');
 exports.createAccommodation = {
   body: Joi.object().keys({
     hotelName: Joi.string().required(),
-    urlTitle: Joi.string().required(),
     location: Joi.string().required(),
     stars: Joi.string().required(),
     hotelTax: Joi.string(),
     hotelService: Joi.string(),
     hotelOrder: Joi.number(),
-    hotelNameAr: Joi.string(),
     distance: Joi.string(),
     time: Joi.string(),
-    timeInArabic: Joi.string(),
-    entryDate: Joi.date(),
-    expirationDate: Joi.date(),
     hotelRooms: Joi.array().items({
       roomCategory: Joi.string().required(),
       roomCategoryInArabic: Joi.string().required(),
@@ -25,7 +20,6 @@ exports.createAccommodation = {
       currency: Joi.string().max(10).default('JD'),
       available: Joi.number(),
     }),
-    hotelImages: Joi.array().items(Joi.string()).optional(),
   }),
 };
 
